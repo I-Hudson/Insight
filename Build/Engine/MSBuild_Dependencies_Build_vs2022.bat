09329be89896948519e81e9c186645255143d498
I-Hudson <cuffy94@gmail.com>
2023-04-11 20:02:19 +0100

    Build all insight related solutions.

--- Build/Engine/MSBuild_Dependencies_Build_vs2022.bat
+++ Build/Engine/MSBuild_Dependencies_Build_vs2022.bat
@@ -1,3 +1,10 @@
 call Generate_Projects_vs2019.bat
+
 call Build_Solution.bat ../../Insight.sln vs2019 Build Debug win64
 call Build_Solution.bat ../../Insight.sln vs2019 Build Release win64
+
+call Build_Solution.bat ../../InsightStandalone.sln vs2019 Build Debug win64
+call Build_Solution.bat ../../InsightStandalone.sln vs2019 Build Release win64
+
+call Build_Solution.bat ../../InsightReflectTool.sln vs2019 Build Debug win64
+call Build_Solution.bat ../../InsightReflectTool.sln vs2019 Build Release win64
--- Build/Engine/MSBuild_Dependencies_Rebuild_vs2022.bat
+++ Build/Engine/MSBuild_Dependencies_Rebuild_vs2022.bat
@@ -1,3 +1,9 @@
 call Generate_Projects_vs2019.bat
 call Build_Solution.bat ../../Insight.sln vs2019 Rebuild Debug win64
 call Build_Solution.bat ../../Insight.sln vs2019 Rebuild Release win64
+
+call Build_Solution.bat ../../InsightStandalone.sln vs2019 Rebuild Debug win64
+call Build_Solution.bat ../../InsightStandalone.sln vs2019 Rebuild Release win64
+
+call Build_Solution.bat ../../InsightReflectTool.sln vs2019 Rebuild Debug win64
+call Build_Solution.bat ../../InsightReflectTool.sln vs2019 Rebuild Release win64
