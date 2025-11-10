92a626777fd755ebad8326cdccb7262626c465d9
I-Hudson <I-Hudson@users.noreply.github.com>
2025-11-10 19:32:51 +0000

    Remove PlatformToolset from build scripts

--- Build/Engine/Build_Solution.bat
+++ Build/Engine/Build_Solution.bat
@@ -28,11 +28,10 @@ call :ValididateInput %MSPlatformTypes% "%platform%" "Invalid platform type, val
 set vsSolutionExtension=
 set vsDevCmd=
 echo Finding most recent VS option
-for /f "tokens=1,2 delims=," %%a in (vsDevCmdVersions.txt) do (
+for /f "tokens=1,2,3 delims=," %%a in (vsDevCmdVersions.txt) do (
     if "!vsDevCmd!" == "" (
         if exist "%%a" (
             SET vsDevCmd=%%a
-            echo %%b
             if "%%b" == "vs2026" (
                 set vsSolutionExtension=x
             )
@@ -71,7 +70,7 @@ if not "%outDirectory%" == "" (
 )
 
 set solution=!solution!!vsSolutionExtension!
-msbuild -maxCpuCount /t:%msBuildType% /p:Configuration=%configuration% /p:Platform=%platform% /p:PlatformToolset=%platformToolSet% %msbuildOutDirectory% !solution!
+msbuild -maxCpuCount /t:%msBuildType% /p:Configuration=%configuration% /p:Platform=%platform% %msbuildOutDirectory% !solution!
 GOTO END
 
 :ValididateInput
