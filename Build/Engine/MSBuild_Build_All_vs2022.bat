7e06111f872c47a36c131f8260331ee97d04ebae
I-Hudson <I-Hudson@users.noreply.github.com>
2025-11-18 21:37:08 +0000

    Run reflect tool for all build

--- Build/Engine/MSBuild_Build_All_vs2022.bat
+++ Build/Engine/MSBuild_Build_All_vs2022.bat
@@ -2,6 +2,8 @@ call Generate_Projects_vs2019.bat
 
 call MSBuild_Dependencies_Build_vs2022.bat
 
+call RunInsightReflectTool.bat
+
 call Build_Solution.bat ../../Insight.sln vsany Build Debug win64
 call Build_Solution.bat ../../Insight.sln vsany Build Release win64
 
